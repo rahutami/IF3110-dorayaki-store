7a57c4af59f980cfe97fa43ef023ba983b10a73e
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-10-01 20:18:28 +0700

    changed decrement amount to change amount

--- change-amount.php
+++ change-amount.php
@@ -6,10 +6,14 @@ try{
     if(true){
         // TODO: uncomment once frontend is ok
     // if ($_POST["id"] || $_POST["name"] ) {
-        $stmt = $db->prepare("UPDATE dorayaki SET amount = amount - ? where id = ?");
+        $stmt = $db->prepare("UPDATE dorayaki SET amount = ? ? where id = ?");
 
         // TODO: to be replaced by $_POST
-        $stmt->execute(array(2, 4));
+        // *command for decrement
+        $stmt->execute(array('amount -', 2, 4)); // amount = amount - 
+
+        // *command for changing amount
+        // $stmt->execute(array('', 2, 4)); --> amount = 2
 
         echo $stmt->rowCount();
     }
