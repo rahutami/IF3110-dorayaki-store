01dadd857ef7329260d1fd9d0627f99bbb9d49f1
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-10-21 21:18:08 +0700

    history page + change amount page

--- add-variant-page.php
+++ add-variant-page.php
@@ -1,24 +1,53 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Booksy</title>
+    <link href="../styles/styles.css" rel="stylesheet" />
+    <link href="../styles/history-styles.css" rel="stylesheet" />
 </head>
+
 <body>
-    <form action="add-variant.php" method="POST">
-        <label for="name">Nama Variant: </label>
-        <input type="text" name="name" id="name">
-        <label for="price">Harga: </label>
-        <input type="text" name="price" id="price">
-        <label for="amount">Jumlah: </label>
-        <input type="text" name="amount" id="amount">
-        <label for="deskripsi">Deskripsi: </label>
-        <textarea id="w3review" name="description" rows="4" cols="50"></textarea>
-        <label for="img_path">Gambar: </label>
-        <input type="text" name="img_path" id="img_path">
-        <button>Submit</button>
+    <!-- navbar -->
+    <nav>
+        <a class="logo">Booksy </a>
+        <!-- <a class="logo">Hi, <?php echo $current_user; ?>!</a> -->
+        <a href="index.php">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <!-- <a href="../cart.php">Cart (<?php echo $totalItems; ?>)</a> -->
+        <!-- <a href="../logout.php">Logout</a> -->
+
+    </nav>
+    <!-- product -->
+    <div class="container">
+        <h1>Add New Variant</h1>
+        <form action="add-variant.php" method="POST">
+        <div class="input-set">
+            <label for="name">Nama Variant</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="input-set">
+            <label for="price">Harga</label>
+            <input type="text" name="price" id="price">
+        </div>
+        <div class="input-set">
+            <label for="amount">Jumlah</label>
+            <input type="text" name="amount" id="amount">
+        </div>
+        <div class="input-set">
+            <label for="deskripsi">Deskripsi</label>
+            <textarea id="w3review" name="description" rows="4" cols="50"></textarea>
+        </div>
+        <div class="input-set">
+            <label for="img_path">Gambar</label>
+            <input type="text" name="img_path" id="img_path">
+        </div>
+        <button class="btn-jumbotron">Submit</button>
     </form>
     <div>
         <?php if (!isset($_GET["success"])) { 
@@ -30,5 +59,10 @@
         <?php } ?>
 
     </div>
+    </div>
+    <!-- footer -->
+    <footer>Footer</footer>
 </body>
+<script>
+</script>
 </html>
--- change-amount.php
+++ change-amount.php
@@ -19,17 +19,22 @@ try{
         $stmt_dorayaki = $db->prepare("UPDATE dorayaki SET amount = ? WHERE id = ?");
 
         if($_POST["method"] == "pembelian"){
+
             $new_amount = (int) $dorayaki["amount"] - (int) $_POST["amount"];
-            $stmt_riwayat = $db->prepare("INSERT INTO riwayat_pembelian (id_dorayaki, id_user, amount) VALUES (?,?,?);");
-            $stmt_riwayat->execute(array($_POST["id"], 1, $_POST["amount"]));
+            $total_price = ((int) $dorayaki["price"]) * ((int) $_POST["amount"]);
+            $stmt_riwayat = $db->prepare("INSERT INTO riwayat_pembelian (id_dorayaki, id_user, amount, total_price, buy_time) VALUES (?,?,?,?,datetime());");
+            $stmt_riwayat->execute(array($_POST["id"], 1, $_POST["amount"], $total_price));
             $stmt_dorayaki->execute(array($new_amount, $_POST["id"]));
+
         } else if ($_POST["method"] == "perubahan"){
+
             $new_amount = (int) $_POST["amount"];
             $amount_changed = (int) $_POST["amount"] - (int) $dorayaki["amount"];
-            $stmt_riwayat = $db->prepare("INSERT INTO riwayat_perubahan (id_dorayaki, id_user, amount_changed, new_amount) VALUES (?,?,?,?);");
+            $stmt_riwayat = $db->prepare("INSERT INTO riwayat_perubahan (id_dorayaki, id_user, amount_changed, new_amount, change_time) VALUES (?,?,?,?,datetime());");
             $stmt_riwayat->execute(array($_POST["id"], 1, $amount_changed, $new_amount));
             $stmt_dorayaki->execute(array($new_amount, $_POST["id"]));
             header('Location: change-amount-page.php?id='.$_POST["id"].'&success=true');
+
         } else {
             throw new Exception("method not available", 1);            
         }
@@ -38,7 +43,7 @@ try{
         // TODO: harusnya diredirect
     } else {
         // TODO: harusnya diredirect
-        header('Location: change-amount-page.php?success=false');
+        header('Location: change-amount-page.php?id='.$_POST["id"].'&success=false');
     }
 
 } catch(PDOException $e) {
