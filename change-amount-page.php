9f406784bbcdded8b943e87900823bc99fc0dd39
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-10-21 17:29:26 +0700

    change amount page

---
+++ change-amount-page.php
@@ -0,0 +1,90 @@
+<?php
+require_once('./db/DBConnection.php');
+$db = (new DBConnection())->connect();
+
+// get details of dorayaki
+try {
+    $id = $_GET['id'];
+    $stmt = $db->prepare("SELECT * FROM dorayaki WHERE id = $id");
+    $stmt->execute();
+    $row = $stmt->fetch();
+    $name = $row["name"];
+    $amountSold = ''; // TODO bikin query-nya
+    $price = $row["price"];
+    $amountRemaining = $row["amount"];
+    $description = $row["description"];
+    $imagePath = $row["img_path"];
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+// buy dorayaki
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booksy</title>
+    <link href="../styles/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav>
+        <a class="logo">Booksy </a>
+        <!-- <a class="logo">Hi, <php echo $current_user; ?>!</a> -->
+        <a href="index.php">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <!-- <a href="../cart.php">Cart (?php echo $totalItems; ?>)</a> -->
+        <!-- <a href="../logout.php">Logout</a> -->
+
+    </nav>
+    <!-- product -->
+    <div class="container">
+        <h1 class="headline">Product Details <?php echo $_GET["id"]; ?></h1>
+        <div class="row">
+            <div class="column center">
+                <!-- foto produk -->
+                <img class="product-photo" src="<?php echo $imagePath;?>">
+            </div>
+            <div class="column">
+                <h1 class="product-name"><?php echo $name; ?></h1>
+                <h3 class="product-description">Amount sold: <?php echo $amountSold; ?> TODO</h3> 
+                <h3 class="product-description">Price: Rp<span id="price"><?php echo $price; ?></span></h3>
+                <h3 class="product-description">Amount remaining: <?php echo $amountRemaining; ?></h3>
+
+                <br>
+                <h4>Description:</h4>
+                <p><?php echo $description; ?></p>
+
+                <form method="post" class="cartForm" action="change-amount.php">
+                    <label for="amount">New amount:</label>
+                    <input type="number" id="amount" name="amount" min="0" required>
+                    <input type="text" value=<?php echo $_GET["id"]?> id="id" name="id" class="hide">
+                    <input type="text" value="perubahan" id="method" name="method" class="hide">
+                    <button type="submit" name="submit" class="btn-jumbotron" 
+                style="font-weight: 600;text-transform: uppercase;">Update</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <footer>Footer</footer>
+</body>
+<script>
+    document.getElementById("quantity").addEventListener("change", getTotalPrice);
+    function getTotalPrice() {
+        let price = document.getElementById("price").textContent;
+        let quantity = this.value;
+        let totalPrice = price*quantity;
+        document.getElementById("totalPrice").innerHTML = totalPrice.toString();
+    }
+</script>
+</html>
--- change-amount.php
+++ change-amount.php
@@ -11,7 +11,9 @@ try{
     // * - kalo pembelian itu gamasuk ke riwayat perubahan tapi cuma ke riwayat pembelian, vice versa
     // * - pas perubahan by admin inputnya new amount
     // * - pas pembelian inputnya amount yg dibeli
-    
+
+    echo $_POST["id"];
+
     if ($_POST["id"] && $_POST["amount"] && $_POST["method"]) {
         $dorayaki = $db->query("SELECT * FROM dorayaki where id = " . $_POST["id"])->fetch();
         $stmt_dorayaki = $db->prepare("UPDATE dorayaki SET amount = ? WHERE id = ?");
@@ -20,22 +22,23 @@ try{
             $new_amount = (int) $dorayaki["amount"] - (int) $_POST["amount"];
             $stmt_riwayat = $db->prepare("INSERT INTO riwayat_pembelian (id_dorayaki, id_user, amount) VALUES (?,?,?);");
             $stmt_riwayat->execute(array($_POST["id"], 1, $_POST["amount"]));
+            $stmt_dorayaki->execute(array($new_amount, $_POST["id"]));
         } else if ($_POST["method"] == "perubahan"){
             $new_amount = (int) $_POST["amount"];
             $amount_changed = (int) $_POST["amount"] - (int) $dorayaki["amount"];
             $stmt_riwayat = $db->prepare("INSERT INTO riwayat_perubahan (id_dorayaki, id_user, amount_changed, new_amount) VALUES (?,?,?,?);");
             $stmt_riwayat->execute(array($_POST["id"], 1, $amount_changed, $new_amount));
+            $stmt_dorayaki->execute(array($new_amount, $_POST["id"]));
+            header('Location: change-amount-page.php?id='.$_POST["id"].'&success=true');
         } else {
             throw new Exception("method not available", 1);            
         }
         
-        // *user id nanti didapet from session?
-        $stmt_dorayaki->execute(array($new_amount, $_POST["id"]));
         // echo $stmt->rowCount();
         // TODO: harusnya diredirect
     } else {
         // TODO: harusnya diredirect
-        echo "please include id, amount, and method";
+        header('Location: change-amount-page.php?success=false');
     }
 
 } catch(PDOException $e) {
