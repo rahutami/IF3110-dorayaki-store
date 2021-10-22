0c1691f63e3f11ab43d196cd47899e73c44344d7
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-10-22 05:35:20 +0000

    Update _navbar.php

--- _navbar.php
+++ _navbar.php
@@ -7,31 +7,23 @@ else {
 }
 
 ?>
-<nav>
-    <!-- <a class="logo">Hi, <?php echo $current_user; ?>!</a> -->
-    <div class="menu">
-        <a class="logo">Stand with Dorayaki</a>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <div class="navbar" id="topnav">
-        <a class="active">Stand with Dorayaki <?php echo ($_COOKIE["admin"] == 1) ?  "- Admin": "" ?></a>
-        <a href="dashboard.php">Home</a>
-        <a href="history.php">History</a>
-        <?php if($isAdmin) { ?>
-        <a href="new-variant.php">Add Variant</a>
-        <?php }?>
-    </div>
+
+<div class="navbar" id="topnav">
+    <a class="active">Stand with Dorayaki <?php echo ($_COOKIE["admin"] == 1) ?  "- Admin": "" ?></a>
+    <a href="dashboard.php">Home</a>
+    <a href="history.php">History</a>
+    <?php if($isAdmin) { ?>
+    <a href="new-variant.php">Add Variant</a>
+    <?php }?>
     <form action="search-result.php">
         <input type="text" name="query" id="query">
-        <button type="submit">Search</button>
+        <button type="submit" class="search-btn">Search</button>
     </form>
-    <!-- <a href="../cart.php">Cart (<?php echo $totalItems; ?>)</a> -->
     <a href="logout.php">Logout</a>
-</nav>
-        <a href="logout.php">Logout</a>
-        <a href="javascript:void(0);" class="icon" onclick="responsiveNavbar();">
-            <i class="fa fa-bars"></i>
-        </a>
-    </div>  
+    <a href="javascript:void(0);" class="icon" onclick="responsiveNavbar();">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>  
 
 <script>
     function responsiveNavbar() {
