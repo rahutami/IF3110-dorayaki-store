a01613f176c6bc4eae2818ec437c15d7c59f9d97
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-10-22 11:35:49 +0700

    fixed conflict and merged shafira branch

--- _navbar.php
+++ _navbar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if (isset($_COOKIE["admin"]) &&$_COOKIE["admin"] == 1) {
     $isAdmin = true;
@@ -12,16 +11,13 @@ else {
     <!-- <a class="logo">Hi, <?php echo $current_user; ?>!</a> -->
     <div class="menu">
         <a class="logo">Stand with Dorayaki</a>
-=======
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="navbar" id="topnav">
         <a class="active">Stand with Dorayaki <?php echo ($_COOKIE["admin"] == 1) ?  "- Admin": "" ?></a>
->>>>>>> shafira
         <a href="dashboard.php">Home</a>
         <a href="history.php">History</a>
         <?php if($isAdmin) { ?>
         <a href="new-variant.php">Add Variant</a>
-<<<<<<< HEAD
         <?php }?>
     </div>
     <form action="search-result.php">
@@ -31,7 +27,6 @@ else {
     <!-- <a href="../cart.php">Cart (<?php echo $totalItems; ?>)</a> -->
     <a href="logout.php">Logout</a>
 </nav>
-=======
         <a href="logout.php">Logout</a>
         <a href="javascript:void(0);" class="icon" onclick="responsiveNavbar();">
             <i class="fa fa-bars"></i>
@@ -48,4 +43,3 @@ else {
         }
 }
 </script>
->>>>>>> shafira
