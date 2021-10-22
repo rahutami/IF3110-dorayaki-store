53044249b47be1bd1bc533ead61d6167deb29f95
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-10-22 05:36:14 +0000

    Revert "Update _navbar.php"
    This reverts commit 0c1691f63e3f11ab43d196cd47899e73c44344d7

--- _navbar.php
+++ _navbar.php
@@ -7,23 +7,31 @@ else {
 }
 
 ?>
-
-<div class="navbar" id="topnav">
-    <a class="active">Stand with Dorayaki <?php echo ($_COOKIE["admin"] == 1) ?  "- Admin": "" ?></a>
-    <a href="dashboard.php">Home</a>
-    <a href="history.php">History</a>
-    <?php if($isAdmin) { ?>
-    <a href="new-variant.php">Add Variant</a>
-    <?php }?>
+<nav>
+    <!-- <a class="logo">Hi, <?php echo $current_user; ?>!</a> -->
+    <div class="menu">
+        <a class="logo">Stand with Dorayaki</a>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class="navbar" id="topnav">
+        <a class="active">Stand with Dorayaki <?php echo ($_COOKIE["admin"] == 1) ?  "- Admin": "" ?></a>
+        <a href="dashboard.php">Home</a>
+        <a href="history.php">History</a>
+        <?php if($isAdmin) { ?>
+        <a href="new-variant.php">Add Variant</a>
+        <?php }?>
+    </div>
     <form action="search-result.php">
         <input type="text" name="query" id="query">
-        <button type="submit" class="search-btn">Search</button>
+        <button type="submit">Search</button>
     </form>
+    <!-- <a href="../cart.php">Cart (<?php echo $totalItems; ?>)</a> -->
     <a href="logout.php">Logout</a>
-    <a href="javascript:void(0);" class="icon" onclick="responsiveNavbar();">
-        <i class="fa fa-bars"></i>
-    </a>
-</div>  
+</nav>
+        <a href="logout.php">Logout</a>
+        <a href="javascript:void(0);" class="icon" onclick="responsiveNavbar();">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>  
 
 <script>
     function responsiveNavbar() {
