f1685b076e61aec300107eb1acf5da24eec3a778
Shafira Naya <shfrnaya@gmail.com>
2021-10-22 11:12:44 +0700

    style: add responsive navbar

--- _navbar.php
+++ _navbar.php
@@ -1,9 +1,23 @@
-    <nav>
-        <a class="logo">Stand with Dorayaki</a>
-        <!-- <a class="logo">Hi, <?php echo $current_user; ?>!</a> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class="navbar" id="topnav">
+        <a class="active">Stand with Dorayaki</a>
+        <a>Hi, <?php echo $_COOKIE["name"] ?>!</a>
         <a href="dashboard.php">Home</a>
         <a href="history.php">History</a>
         <a href="new-variant.php">Add Variant</a>
-        <!-- <a href="../cart.php">Cart (<?php echo $totalItems; ?>)</a> -->
         <a href="logout.php">Logout</a>
-    </nav>
+        <a href="javascript:void(0);" class="icon" onclick="responsiveNavbar();">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>  
+
+<script>
+    function responsiveNavbar() {
+        let nav = document.getElementById("topnav");
+        if (nav.className === "navbar") {
+            nav.className += " responsive";
+        } else {
+            nav.className = "navbar";
+        }
+}
+</script>
