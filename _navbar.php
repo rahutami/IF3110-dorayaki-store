da1e1fa95cd809daec367c82a148d879a7228e8d
Shafira Naya <shfrnaya@gmail.com>
2021-10-22 11:28:36 +0700

    style: responsive container

--- _navbar.php
+++ _navbar.php
@@ -1,7 +1,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="navbar" id="topnav">
-        <a class="active">Stand with Dorayaki</a>
-        <a>Hi, <?php echo $_COOKIE["name"] ?>!</a>
+        <a class="active">Stand with Dorayaki <?php echo ($_COOKIE["admin"] == 1) ?  "- Admin": "" ?></a>
         <a href="dashboard.php">Home</a>
         <a href="history.php">History</a>
         <a href="new-variant.php">Add Variant</a>
